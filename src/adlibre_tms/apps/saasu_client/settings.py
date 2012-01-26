fd4ea5cac0e8c261cfc9adfb600dcc1e3124199a
garmoncheg <garmon1@gmail.com>
2012-01-26 16:17:09 +0200

    Fix SAASU disabling by demo mode

--- src/adlibre_tms/apps/saasu_client/settings.py
+++ src/adlibre_tms/apps/saasu_client/settings.py
@@ -1,10 +1,16 @@
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
+SAASU_ERORS = {
+    'no_key': 'Please set your SAASU_WSACCESS_KEY setting.',
+    'no_uid': 'Please set your SAASU_FILE_UID setting.',
+    'disabled': 'Disabled in demo mode'
+}
+
 SAASU_WSACCESS_KEY = getattr(settings, 'SAASU_WSACCESS_KEY', None)
 if SAASU_WSACCESS_KEY is None:
-    raise ImproperlyConfigured('Please set your SAASU_WSACCESS_KEY setting.')
+    raise ImproperlyConfigured(SAASU_ERORS['no_key'])
 
 SAASU_FILE_UID = getattr(settings, 'SAASU_FILE_UID', None)
 if SAASU_FILE_UID is None:
-    raise ImproperlyConfigured('Please set your SAASU_FILE_UID setting.')
+    raise ImproperlyConfigured(SAASU_ERORS['no_uid'])
--- src/adlibre_tms/apps/tms/contrib/saasu/views.py
+++ src/adlibre_tms/apps/tms/contrib/saasu/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from django.utils.translation import ugettext_lazy as _
+from django.conf import settings
+from django.http import HttpResponse
 
+from saasu_client.settings import SAASU_ERORS
 from saasu_client.models import ContactList, FullInventoryItemList, TransactionCategoryList
 
 
 @login_required
 def saasu_account_lookup(request, app_label="SAASU", template_name='saasu/admin/saasu_account_lookup.html'):
     is_popup = False
+    if settings.DEMO:
+        return HttpResponse(SAASU_ERORS['disabled'])
     if request.method == "GET":
         if request.GET.has_key('pop') or request.GET.has_key('popup'):
             is_popup = True
@@ -22,6 +27,8 @@ def saasu_account_lookup(request, app_label="SAASU", template_name='saasu/admin/
 @login_required
 def saasu_contact_lookup(request, app_label="SAASU", template_name='saasu/admin/saasu_contact_lookup.html'):
     is_popup = False
+    if settings.DEMO:
+        return HttpResponse(SAASU_ERORS['disabled'])
     if request.method == "GET":
         if request.GET.has_key('pop') or request.GET.has_key('popup'):
             is_popup = True
@@ -35,6 +42,8 @@ def saasu_contact_lookup(request, app_label="SAASU", template_name='saasu/admin/
 @login_required
 def saasu_item_lookup(request, app_label="SAASU", template_name='saasu/admin/saasu_item_lookup.html'):
     is_popup = False
+    if settings.DEMO:
+        return HttpResponse(SAASU_ERORS['disabled'])
     if request.method == "GET":
         if request.GET.has_key('pop') or request.GET.has_key('popup'):
             is_popup = True
