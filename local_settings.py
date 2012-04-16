44674c671f629f8e209c012f24fb02f166e69e16
Andrew Cutler <macropin@gmail.com>
2012-04-16 16:12:30 +1000

    Try some extra magic

--- local_settings.py
+++ local_settings.py
@@ -1,13 +1,20 @@
 #### Database Location ../db/
 
 import os
+PROJECT_PATH = os.path.abspath(os.path.split(__file__)[0])
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.abspath(os.path.join(os.path.split(__file__)[0], '..', 'db', 'tms.sqlite')),
+        'NAME': os.path.abspath(os.path.join(PROJECT_PATH, '..', 'db', 'tms.sqlite')),
         }
 }
+
+MEDIA_ROOT = os.path.abspath(os.path.join(PROJECT_PATH, '..', 'www', 'media'))
+STATIC_ROOT = os.path.abspath(os.path.join(PROJECT_PATH, '..', 'www', 'static'))
+
+STATICFILES_DIRS = ()
+
 ###
 
 ADMINS = (
