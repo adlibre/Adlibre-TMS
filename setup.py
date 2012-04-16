0ec369a48b7fe828430a4038269741b87808e84b
Andrew Cutler <macropin@gmail.com>
2012-04-16 13:24:02 +1000

    Test new deployment method

--- setup.py
+++ setup.py
@@ -7,15 +7,19 @@ setup(name='adlibre_tms',
     long_description=open('README.md').read(),
     url='https://github.com/macropin/Adlibre-TMS',
     packages=['adlibre_tms'],
-    scripts=['adlibre_tms/manage.py',],
+    scripts=[],
+    data_files = [
+            ('tms', ['adlibre_tms/local_settings.py', 'adlibre_tms/manage.py', 'adlibre_tms/deployment/manage-fcgi.sh']),
+        ],
     install_requires=[
-        'BeautifulSoup==3.2.0',
-        'Django==1.3.1',
-        'django-any==0.2.0',
-        'django-compressor==1.1.1',
-        'django-pagination==1.0.7',
-        'django-uni-form==0.7.0',
-        'flup==1.0.3.dev-20110405'
+            'BeautifulSoup==3.2.0',
+            'Django==1.3.1',
+            'django-any==0.2.0',
+            'django-compressor==1.1.1',
+            'django-pagination==1.0.7',
+            'django-uni-form==0.7.0',
+            'flup==1.0.3.dev-20110405',
+            'xml-models==0.5.1'
         ],
     dependency_links = [
         ],
