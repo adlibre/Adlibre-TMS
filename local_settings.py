a20acc9a50b31ac879845adb94a16a2bc2e0ad5e
Andrew Cutler <macropin@gmail.com>
2012-04-18 21:06:02 +1000

    Move some settings to our settings.py file (ones which override our settings.py in adlibre_tms source dir).

--- local_settings.py
+++ local_settings.py
@@ -1,20 +1,3 @@
-#### Database Location ../db/
-
-import os
-PROJECT_PATH = os.path.abspath(os.path.split(__file__)[0])
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.abspath(os.path.join(PROJECT_PATH, '..', 'db', 'tms.sqlite')),
-        }
-}
-
-MEDIA_ROOT = os.path.abspath(os.path.join(PROJECT_PATH, '..', 'www', 'media'))
-STATIC_ROOT = os.path.abspath(os.path.join(PROJECT_PATH, '..', 'www', 'static'))
-
-###
-
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
@@ -23,11 +6,11 @@ ADMINS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'change me'
+#SECRET_KEY = 'change me'
 
 # Saasu settings
-SAASU_FILE_UID = 'XXXX'
-SAASU_WSACCESS_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXX'
+#SAASU_FILE_UID = 'XXXX'
+#SAASU_WSACCESS_KEY = 'XXXXXXXXXXXXXXXXXXXXXXXXX'
 
 # specific timezone settings
 TIME_ZONE = None
