38cfbe7d14a5b8448d8e9e191dad5381d83ad916
Andrew Cutler <macropin@gmail.com>
2014-05-12 16:19:31 +1000

    Use cling

--- adlibre_tms/wsgi.py
+++ adlibre_tms/wsgi.py
@@ -21,7 +21,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "adlibre_tms.settings")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling, MediaCling
+
+application = Cling(MediaCling(get_wsgi_application()))
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
