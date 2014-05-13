2303b68005a6530b61e90b1fb977465a9cd5da01
Andrew Cutler <macropin@gmail.com>
2014-05-13 14:38:33 +1000

    typo

--- adlibre_tms/apps/saasu_client/settings.py
+++ adlibre_tms/apps/saasu_client/settings.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
-SAASU_ERORS = {
+SAASU_ERRORS = {
     'no_key': 'Please set your SAASU_WSACCESS_KEY setting.',
     'no_uid': 'Please set your SAASU_FILE_UID setting.',
     'disabled': 'Disabled in demo mode'
@@ -9,8 +9,8 @@ SAASU_ERORS = {
 
 SAASU_WSACCESS_KEY = getattr(settings, 'SAASU_WSACCESS_KEY', None)
 if SAASU_WSACCESS_KEY is None:
-    raise ImproperlyConfigured(SAASU_ERORS['no_key'])
+    raise ImproperlyConfigured(SAASU_ERRORS['no_key'])
 
 SAASU_FILE_UID = getattr(settings, 'SAASU_FILE_UID', None)
 if SAASU_FILE_UID is None:
-    raise ImproperlyConfigured(SAASU_ERORS['no_uid'])
+    raise ImproperlyConfigured(SAASU_ERRORS['no_uid'])
