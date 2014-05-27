c67f4dab9243e0cf3db6ef7dac7cd4f327952bac
Andrew Cutler <andrew@adlibre.com.au>
2014-05-27 13:35:48 +1000

    Deploy bureaucrat requirements

--- setup.py
+++ setup.py
@@ -105,9 +105,9 @@ setup(
             ], # this should be done automatically
         },
     data_files=[
+        ('', ['.env.example', 'Deployfile', 'Procfile']),
         ('adlibre_tms', ['settings_prod.py', 'local_settings.py.example', 'manage.py']),
         ('db', ['db/.gitignore']), # create empty dir
-        ('deployment', find_files('deployment', '*')),
         ('docs', find_files('docs', '*')),
         ('www', ['www/.gitignore']), # create empty dir
     ],
