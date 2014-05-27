7d57f7b048b2816d61d0a6088903e8196e861b2a
Andrew Cutler <andrew@adlibre.com.au>
2014-05-27 13:48:41 +1000

    revert

--- setup.py
+++ setup.py
@@ -106,7 +106,7 @@ setup(
         },
     data_files=[
         ('', ['.env.example', 'requirements.txt', 'Deployfile', 'Procfile']),
-        ('', ['settings_prod.py', 'local_settings.py.example', 'manage.py']),
+        ('adlibre_tms', ['settings_prod.py', 'local_settings.py.example', 'manage.py']),
         ('db', ['db/.gitignore']), # create empty dir
         ('docs', find_files('docs', '*')),
         ('www', ['www/.gitignore']), # create empty dir
